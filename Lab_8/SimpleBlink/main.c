5391e64a429cf142dc5c38cd9767491e74856dc8
Tim Roche Laptop <kc2vsa@gmail.com>
2019-10-31 12:28:26 -0400

    Finished Lab 8

---
+++ Lab_8/SimpleBlink/main.c
@@ -0,0 +1,111 @@
+#include <msp430.h>
+//HARDWARE
+/*
+int main(void){
+    WDTCTL = WDTPW + WDTHOLD;           // Stop WDT
+
+    P1SEL |= BIT2;
+    P1DIR |= BIT2;
+
+    TA0CTL = TASSEL_2 + MC_1 + TACLR;   // SMCLK, up mode
+    TA0CCR0 = 511;                    // Sets the PWM Period
+    TA0CCTL1 = OUTMOD_7;                // CCR1 reset/set
+    TA0CCR1 = 0;                      // CCR1 PWM duty cycle
+
+    //Button part
+    P1DIR &= ~BIT1;             // Sets P1.3 in the input direction
+    P1REN |= BIT1;              // P1.3 pullup/pulldown resistor enabled
+    P1OUT |= BIT1;              // P1.3 pullup/pulldown configured as
+
+    P1IE |= BIT1;               // P1.3 interrupt enabled
+    P1IES |= BIT1;              // P1.3 interrupt flag is set with a high
+                                // to low transition
+    P1IFG &= ~BIT1;             // P1.3 interrupt flag is cleared
+    _BIS_SR(LPM0_bits + GIE);   // Sets the processor to low processor mode
+                                // and enables global interrupts
+}
+
+#pragma vector = PORT1_VECTOR
+__interrupt void button_interrupt(void){
+   if (TA0CCR1 <= 510){        // If duty cycle is not 100%
+       TA0CCR1 += 51;          // Increase duty cycle by 10%
+   } else{                     // If not
+       TA0CCR1 = 0;            // Sets duty cycle to 0%
+   }
+   P1IFG &= ~BIT1;             // P1.3 interrupt flag is cleared
+}
+*/
+
+//SOFTWARE
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
+    //UCSCTL4 = SELA_0;           //
+
+    //P1SEL &= ~(BIT0 + BIT3);    // Selects I/O function
+    //P1SEL2 &= ~(BIT0 + BIT3);   // Selects I/O function
+
+    //LED
+    P1DIR |= BIT0;              // Sets P1.0 in the output direction
+    P1OUT &= ~BIT0;             // Clears P1.0 output register value
+
+
+    //BUTTON
+    P1DIR &= ~BIT1;             // Sets P1.1 in the input direction
+    P1REN |= BIT1;              // P1.1 pullup/pulldown resistor enabled
+    P1OUT |= BIT1;              // P1.1 pullup/pulldown configured as
+                                // pull up resistor
+
+    P1IE |= BIT1;               // P1.1 interrupt enabled
+    P1IES |= BIT1;              // P1.1 interrupt flag is set with a high
+                                // to low transition
+    P1IFG &= ~BIT1;             // P1.1 interrupt flag is cleared
+
+    // Timer
+    TA0CTL = TASSEL_2 + ID_2 + MC_1 + TACLR + TAIE;
+                                // Sets TA clock to SMCLK, up mode, sets
+                                // the internal divider to 4, and 4, clears
+                                // the clock, and enables interrupts
+    TA0CCR0 = 250;              // Sets the PWM period
+    TA0CCR1 = 150;              // Sets the duty cycle
+    TA0CCTL1 = CCIE;            //
+
+    _BIS_SR(LPM0_bits + GIE);   // Sets the processor to low processor mode
+                                // and enables global interrupts
+
+    return 0;
+}
+
+#pragma vector = PORT1_VECTOR
+__interrupt void button_interrupt(void){
+    if(P1IES & BIT1){           // Detects falling edge
+        TA0CCR1 += 25;          // Increases duty cycle by %10
+
+        if(TA0CCR1 > 250){      // If duty cycle is over 100%
+            TA0CCR1 = 0;        // Sets duty cycle to 0
+            P1OUT &= ~BIT0;     // Shuts off the LED
+            TA0CTL = TACLR;     // Clears the timer
+        }else{
+            TA0CTL = TASSEL_2 + ID_2 + MC_1 + TAIE;
+                                // Sets TA clock to SMCLK, up mode, sets
+                                // the internal divider to 4, and 4, clears
+                                // the clock, and enables interrupts
+        }
+    }
+
+    P1IES ^= BIT1;              //Toggles edge select
+    P1IFG &= ~BIT1;             //Clears interrupt flag
+}
+
+#pragma vector = TIMER0_A1_VECTOR;
+__interrupt void Timer0A0 (void){
+    switch(TA0IV){
+        case 2:                 //
+            P1OUT &= ~BIT0;     // Turning off the LED for button state
+        break;
+        case 14:                 // Overflow detect
+            P1OUT |= BIT0;
+        break;
+    }
+}
+
